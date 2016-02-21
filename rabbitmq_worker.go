f566bd69a9de250a32f32563e47ff4d1e887610c
vvv-v13 <vetal13@gmail.com>
2016-02-21 10:42:20 +0200

    Worker for http_json_to_rabbitmq.go

---
+++ rabbitmq_worker.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+const (
+	RabbitMQUrl  = "amqp://test:test@rabbit:5672//test"
+	RabbitMQueue = "task_queue"
+)
+
+type DataStruct struct {
+	Url string
+	Id  string
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial(RabbitMQUrl)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		RabbitMQueue, // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			d.Ack(false)
+
+			buf := bytes.NewBuffer(d.Body)
+
+			var m DataStruct
+
+			decoder := json.NewDecoder(buf)
+			err := decoder.Decode(&m)
+			failOnError(err, "Failed convert JSON to struct")
+
+			worker(m)
+
+			log.Printf("Done")
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func worker(m DataStruct) {
+	log.Println("Message:", m)
+}
